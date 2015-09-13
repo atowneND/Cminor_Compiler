33e1a63a73a0b8a0837fa8da6f200bf3fd2e98a4
atowneND <atowne@nd.edu>
2015-09-13 14:15:49 -0400

    yylex() runs forever - possibly on reading the next buffer

--- Makefile
+++ Makefile
@@ -1,14 +1,17 @@
 all: cminor
 
 cminor: main.c lex.yy.o token.o
-	gcc main.c lex.yy.o token.o -o cminor -lfl
+	gcc -g main.c lex.yy.o token.o -o cminor -lfl
 
-lex.yy.o:
-	gcc -c lex.yy.c
+lex.yy.o: scanner.l
+	flex --header-file=lex.yy.h scanner.l
+	gcc -g -c lex.yy.c
 
-token.o:
-	gcc -c token.c
+token.o: token.c
+	gcc -g -c token.c
 
 clean:
+	rm lex.yy.c
+	rm lex.yy.h
 	rm *.o
 	rm cminor
--- main.c
+++ main.c
@@ -5,11 +5,20 @@
 #include "lex.yy.h"
 
 int main(int argc, char *argv[]){
-    if(argc!=2){
-        printf("enter one filename\n");
-        exit(1);
-    }
-    char *filename = argv[1];
+//    if(argc!=2){
+//        printf("enter one filename\n");
+//        exit(1);
+//    }
+//    char *filename = argv[1];
+
+    // TEST CASE
+    char *filename = "testfoo";
+    FILE *yyin = fopen(filename,"r");
+    int retval = yylex();
+    // yyin - file with symbols to be read
+    // yylex - scans input, runs machinery, returns one integer for one token type
+    // yytext - global array to look at token
+    
     printf("filename=%s\n",filename);
     return 0;
 }
