4abc9594339f6507a4ba88f8a3ff102f76181cf5
atowneND <atowne@nd.edu>
2015-09-14 13:40:04 -0400

    accepts a narrow range of inputs

--- main.c
+++ main.c
@@ -6,36 +6,39 @@
 
 int main(int argc, char *argv[]){
     // parse input
-//    if (argc==1){
-//        printf("usage: ./cminor -scan <filename>\nPlease enter an option and its arguments\n");
-//        exit(1);
-//    }
-//    else if (argc==2){
-//        printf("usage: ./cminor -scan <filename>\nPlease enter a filename\n");
-//        exit(1);
-//    }
-//    else if (argc!=3){
-//        printf("usage: ./cminor -scan <filename>\n");
-//        exit(1);
-//    }
-//    if (strcmp(argv[2],"-scan")){
-//        char *filename=argv[2];
-//        printf("filename=%s\n",filename);
-//    }
-//    exit(1);
+    if (argc==1){
+        printf("usage: ./cminor -scan <filename>\nPlease enter an option and its arguments\n");
+        exit(1);
+    }
+    else if (argc==2){
+        printf("usage: ./cminor -scan <filename>\nPlease enter a filename\n");
+        exit(1);
+    }
+    else if (argc!=3){
+        printf("usage: ./cminor -scan <filename>\n");
+        exit(1);
+    }
+    if (!strcmp(argv[2],"-scan")){
+        printf("usage: ./cminor -scan <filename>\nno flags other than -scan allowed at this time\n");
+        exit(1);
+    }
+    char *filename = argv[2];
+    yyin = fopen(filename,"r");
+    if (yyin==NULL){
+        printf("Could not open file\n");
+        exit(1);
+    }
 
     // Scan
-    char *filename = "scannerTests/test1";
-    yyin = fopen(filename,"r");
     int retval;
     while(1){
         retval = yylex();
 	    if(!retval){
-	        printf("EOF\n");
+	        printf("EOF");
 	        break;
         }
         else{
-            printf("yy: %s\n",token_string(retval));
+            printf("%s\n",token_string(retval));
         }
     }
 	fclose(yyin);
