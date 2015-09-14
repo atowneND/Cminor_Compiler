5abd60f9c8c462aa8d99bfee14fd0312f408dd92
atowneND <atowne@nd.edu>
2015-09-14 15:14:44 -0400

    added testing

---
+++ tests/good0.sol
@@ -0,0 +1,25 @@
+string type
+identifier
+array
+boolean type
+character type
+else
+false
+for
+function call
+if
+integer type
+print
+return
+string type
+true
+void
+while
+identifier
+identifier
+integer
+integer
+character
+string
+string
+EOF
