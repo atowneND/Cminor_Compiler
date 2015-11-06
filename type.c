62f2be6cb41d76898eb81f23332fe7220421294f
atowneND <atowne@nd.edu>
2015-11-06 04:20:53 -0500

    everything works except arrays

--- type.c
+++ type.c
@@ -18,44 +18,42 @@ struct type * type_create( type_kind_t kind, struct param_list *params, struct t
 
 void type_print( struct type *t ) {
     if (t != NULL) {
-        if (t->kind != 0) {
-            switch (t->kind){
-                case (TYPE_BOOLEAN):
-                    printf("boolean");
-                    break;
-                case (TYPE_CHARACTER):
-                    printf("character");
-                    break;
-                case (TYPE_INTEGER):
-                    printf("integer");
-                    break;
-                case (TYPE_STRING):
-                    printf("string");
-                    break;
-                case (TYPE_ARRAY):
-                    printf("array [");
-                    if (t->expr != 0){
-                        expr_print(t->expr);
-                    }
-                    printf("] ");
-                    if (t->subtype != 0){ 
-                        type_print(t->subtype);
-                    }
-                    break;
-                case (TYPE_FUNCTION):
-                    printf("function ");
+        switch (t->kind){
+            case (TYPE_BOOLEAN):
+                printf("boolean");
+                break;
+            case (TYPE_CHARACTER):
+                printf("char");
+                break;
+            case (TYPE_INTEGER):
+                printf("integer");
+                break;
+            case (TYPE_STRING):
+                printf("string");
+                break;
+            case (TYPE_ARRAY):
+                printf("array [");
+                if (t->expr != 0){
+                    expr_print(t->expr);
+                }
+                printf("] ");
+                if (t->subtype != 0){ 
                     type_print(t->subtype);
-                    printf(" (");
-                    if (t->params){ 
-                        param_list_print(t->params);
-                    }
-                    printf(")");
-                    //indent += 1;
-                    break;
-                case (TYPE_VOID):
-                    printf("void");
-                    break;
-            }
+                }
+                break;
+            case (TYPE_FUNCTION):
+                printf("function ");
+                type_print(t->subtype);
+                printf(" (");
+                if (t->params){ 
+                    param_list_print(t->params);
+                }
+                printf(")");
+                //indent += 1;
+                break;
+            case (TYPE_VOID):
+                printf("void");
+                break;
         }
     }
 }
